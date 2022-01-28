2ae53fe991c57799ba6af35863531604ba7a4c10
Marco R <mprizzuto88@gmail.com>
2022-01-28 11:41:32 -0500

    update exercises array

--- data/exercise-data.php
+++ data/exercise-data.php
@@ -1,9 +1,49 @@
 <?php 
 $exerciseList = [
 	[
-		"name" => "sayng hello",
-		"goal" => "use PHP to say hello",
-		"path" => "exercises/saying-hello.php",
+		"name" => "saying hello",
+		"goal" => "use PHP to say hello. something longer will go here, but i need content to test the layour",
+		"technologies" => "HTML, PHP, CSS",
+		"link" => "exercises/saying-hello.php",
+	],
+	[
+		"name" => "counting characters",
+		"goal" => "create a program that counts the amount of characters in a string",
+		"technologies" => "HTML, PHP, CSS",
+		"link" => "exercises/",
+	],
+	[
+		"name" => "printing quotes",
+		"goal" => "write a program that prompts for an author and quote. output the author and quote",
+		"technologies" => "HTML, PHP, CSS",
+		"link" => "exercises/",
+	],
+	[
+		"name" => "madlibs",
+		"goal" => "write a program that creates a madlib using a noun, verb, adjerb, and adjective. Include them in a string and make a story out of it",
+		"technologies" => "HTML, PHP, CSS",
+		"link" => "exercises/",
+	],
+	[
+		"name" => "simple math",
+		"goal" => "write a program that applies the four mathematical operations to 2 numbers",
+		"technologies" => "HTML, PHP, CSS",
+		"link" => "exercises/",
+	],
+	[
+		"name" => "retirement calculator",
+		"goal" => "create a program that calculates how many years you have left until retirement, and the retirement age",
+		"technologies" => "",
+		"link" => "exercises/",
 	],
 ];
+
+/*
+[
+		"name" => "",
+		"goal" => "",
+		"technologies" => "",
+		"link" => "exercises/",
+	],
+*/ 
 ?>
