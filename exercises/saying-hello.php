21eb11081a064e873c7618d53a778cea23f650a3
Marco R <mprizzuto88@gmail.com>
2022-01-28 18:14:54 -0500

    add heading and paragraph

--- exercises/saying-hello.php
+++ exercises/saying-hello.php
@@ -5,6 +5,9 @@ $errorMessage = "";
 		$_POST["name"] = "undefined";
 	}
 ?>
+<h1>saying hello</h1>
+
+<p>type your name and get a greeting!</p>
 <form method="POST" action="?page=exercise-result&result=saying-hello">
 	<label for="name">name
 		<!-- /pages/exercise-result.php -->
