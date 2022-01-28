23f27fd048e13d77d10b3c637e2bc0a2e3686546
Marco R <mprizzuto88@gmail.com>
2022-01-28 16:42:29 -0500

    incude exercises/detail page logic into router

--- index.php
+++ index.php
@@ -4,30 +4,54 @@ require "functions.php";
 // dynamically generate meta based on query strings
 generateMeta($page);
 require "components/header.php"; 
-// formatInput($page);
+// formatInput($_GET);
 
 
 $exercise = $_GET["exercise"] ?? null;
 // formatInput($exercise);
+
+if (in_array("exercise-result", $_GET)) {
+	$displayContent = "home-page-hidden";
+}
 ?>
 
 <main class="site-main">
-	
 	<?php 
 	//main page router
 	switch($page) {
 		case "exercises":
-		include "pages/exercises.php";
-		break;
+			include "pages/exercises.php";
+			break;
 
 		case "exercise-detail";
-		include "pages/exercise-detail.php";
-		break;
+			include "pages/exercise-detail.php";
+			break;
 
 		default:
-		include "pages/home.php";
+			include "pages/home.php";
 	}
 	?>
+
+	<?php 
+	/*
+		programming exercise router. 
+		only runs if exercise-result query string is in $_GET superglobal array
+	*/
+	if ( in_array("exercise-result", $_GET) ) {
+		//main page router
+		switch($page) {
+			case "exercise-result";
+				include "pages/exercise-result.php";
+				break;
+
+			default:
+		include "pages/404.php";
+				echo "wrong page, go home!";
+		}
+	}
+
+	?>
+	
 </main>
 
 <?php require "components/footer.php"; ?>
