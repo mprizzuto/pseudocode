74b910a0f814a2cdf72a0ee8fb8107fac809cef4
Marco R <mprizzuto88@gmail.com>
2022-01-28 18:04:21 -0500

    write elseif for form validation logic

--- pages/exercise-result.php
+++ pages/exercise-result.php
@@ -11,12 +11,13 @@ require_once "./functions.php";
 
 		<?php foreach($_POST as $value): ?>
 			
-			
-
 		<?php 
 		if (in_array("", $_POST) || ctype_space($_POST["name"])) {
 			echo "<p>you entered NOTHING! try again, or remain unnamed</p>";
-		} else {
+		} elseif (onlyLetters($_POST["name"]) !== 0) {
+			echo "<p>letters only</p>";
+		}
+		else {
 			echo "<p>hello <mark>$value</mark> how are you?</p>";
 		}
 		?>
