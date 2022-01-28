aee8448bad07d9ceef43bcd3a8e0fb8370a9a71b
Marco R <mprizzuto88@gmail.com>
2022-01-28 16:41:39 -0500

    add form logic for whitespace

--- exercises/saying-hello.php
+++ exercises/saying-hello.php
@@ -1,1 +1,16 @@
-saying-hello.php
+<?php 
+$errorMessage = "";
+	if (in_array("", $_POST)) {
+		$errorMessage = "required";
+		$_POST["name"] = "undefined";
+	}
+?>
+<form method="POST" action="?page=exercise-result&result=saying-hello">
+	<label for="name">name
+		<!-- /pages/exercise-result.php -->
+		<input type="text" name="name"><?=$errorMessage?>
+	</label>
+
+	<input type="submit">
+</form>
+
